85d1b59d6a98ece2c9dd4a2b9a8227a73b044a07
bezael <bezael@gmail.com>
2023-01-27 15:59:44 +0100

    feat(Character): Add width and H to image

--- src/components/character/character.tsx
+++ src/components/character/character.tsx
@@ -5,7 +5,7 @@ import type { Character } from '~/models/character';
 export default component$(({ name, image, id }: Pick<Character, 'name' | 'image' | 'id'>) => {
   return (
     <div class='card'>
-      <img src={image} alt={name} class='img-character' />
+      <img src={image} alt={name} class='img-character' width='100%' height='100%' />
       <div>
         <div class='text-center'>
           <Link href={`details/${id}`}>{name}</Link>
